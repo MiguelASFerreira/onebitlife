7c582082d0d2254545eb8ac8b9051110c127846d
Miguel <migant1306@gmail.com>
2022-12-02 11:17:19 -0300

    Create habit e estrutura de rotas

--- src/pages/HabitPage/index.jsx
+++ src/pages/HabitPage/index.jsx
@@ -10,8 +10,9 @@ import {
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export default function HabitPage() {
+export default function HabitPage({ route}) {
   const navigation = useNavigation();
+  const { create, habit } = route.params;
 
   return (
     <View style={styles.container}>
@@ -27,7 +28,11 @@ export default function HabitPage() {
             />
           </TouchableOpacity>
           <View style={styles.mainContent}>
-
+            <Text style={styles.title}>Configurações {"\n"} de hábito</Text>
+            <Text style={styles.inputText}>Área</Text>
+            <View style={styles.inputContainer}>
+              <Text style={styles.area}>{habit?.habitArea}</Text>
+            </View>
           </View>
         </View>
       </ScrollView>
@@ -36,21 +41,45 @@ export default function HabitPage() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "rgba(21, 21, 21, 0.98)",
-    },
-    bakcPageBtn: {
-      width: 40,
-      height: 40,
-      margin: 25,
-    },
-    arrowBack: {
-      width: 40,
-      height: 40,
-    },
-    mainContent: {
-      width: 250,
-      alignSelf: "center",
-    },
-  })
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  bakcPageBtn: {
+    width: 40,
+    height: 40,
+    margin: 25,
+  },
+  arrowBack: {
+    width: 40,
+    height: 40,
+  },
+  mainContent: {
+    width: 250,
+    alignSelf: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 30,
+  },
+  inputText: {
+    color: "#fff",
+    fontSize: 16,
+    marginTop: 35,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: "#FFF",
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  area: {
+    color: "#BBB",
+    fontSize: 15,
+  },
+});
