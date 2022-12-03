cdb1f195bfe300be1aa6515895a8723ab756c4d2
Miguel <migant1306@gmail.com>
2022-12-03 16:37:21 -0300

    Update e Exclude Button

--- src/pages/HabitPage/index.jsx
+++ src/pages/HabitPage/index.jsx
@@ -14,6 +14,8 @@ import SelectHabit from "../../components/HabitPage/SelectHabit";
 import SelectFrequency from "../../components/HabitPage/SelectFrequency";
 import Notification from "../../components/HabitPage/Notification";
 import TimeDatePicker from "../../components/HabitPage/TimeDataPicker";
+import UpdateExcludeButtons from "../../components/HabitPage/UpdateExcludeButtons";
+import DefaultButton from "../../components/common/DefaultButton";
 
 export default function HabitPage({ route }) {
   const navigation = useNavigation();
@@ -24,6 +26,47 @@ export default function HabitPage({ route }) {
   const [timeNotification, setTimeNotification] = useState();
 
   const { create, habit } = route.params;
+
+  function handleCreateHabit() {
+    if (
+      habitInput === undefined ||
+      frequencyInput === undefined
+    ) {
+      Alert.alert(
+        "Você precisa selecionar um hábito e frequência para continuar"
+      );
+    } else if (
+      notificationToggle === true &&
+      frequencyInput === "Diário" &&
+      timeNotification === undefined
+    ) {
+      Alert.alert("Você precisa dizer o horário da notificação!");
+    } else if (
+      notificationToggle === true &&
+      frequencyInput === "Diário" &&
+      dayNotification === undefined &&
+      timeNotification === undefined
+    ) {
+      Alert.alert(
+        "Você precisa dizer a frequência e o horário da notificação!"
+      );
+    } else {
+	     navigation.navigate("Home", {
+         createdHabit: `Created in ${habit?.habitArea}`,
+       });
+    }
+  }
+
+  function handleUpdateHabit() {
+    if (notificationToggle === true && !dayNotification && !timeNotification) {
+      Alert.alert("Você precisa colocar a frequência e horário da notificação");
+    } else {
+      navigation.navigate("Home", {
+        updatedHabit: `Updated in ${habit?.habitArea}`,
+      });
+    }
+  }
+
 
   return (
     <View style={styles.container}>
@@ -77,6 +120,23 @@ export default function HabitPage({ route }) {
                 />
               )
             ) : null}
+
+            {create === false ? (
+              <UpdateExcludeButtons
+                handleUpdate={handleUpdateHabit}
+                habitArea={habitArea}
+                habitInput={habitInput}
+              />
+            ) : (
+              <View style={styles.configButton}>
+                <DefaultButton 
+                  buttonText={"Criar"}
+                  handlePress={handleCreateHabit}
+                  width={250}
+                  height={50}
+                />
+              </View>
+            )}
           </View>
         </View>
       </ScrollView>
